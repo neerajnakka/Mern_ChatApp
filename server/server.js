670293c9e52de4265df689b8fff888edcefa1754
neerajnakka <neerajnakka8@gmail.com>
2024-04-30 14:14:05 +0530

    project end

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+import path from 'path';
 import express from 'express';
 import { app, server } from './socket/socket.js';
 import dotenv from 'dotenv';
@@ -8,6 +9,7 @@ import userRoutes from './routes/user.routes.js';
 import connectToMongoDB from './database/connectToMdb.js';
 
 const PORT_URL = process.env.PORT || 5000;
+const __dirname = path.resolve();
 dotenv.config();
 
 app.use(express.json());
@@ -15,7 +17,11 @@ app.use(cookieParser());
 app.use('/api/auth', authRoutes);
 app.use('/api/messages', messageRoutes);
 app.use('/api/users', userRoutes);
+app.use(express.static(path.join(__dirname, '/client/dist')));
 
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+});
 server.listen(PORT_URL, () => {
   connectToMongoDB();
   console.log(`listening on ${PORT_URL} successfully`);
